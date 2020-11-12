1ecb67ed3a4ea7ad5b0f1f3ad7377df668bea768
mathlete01 <msallin@gmail.com>
2020-11-11 22:04:55 -0800

    Working version before I add authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_10_003350) do
+ActiveRecord::Schema.define(version: 2020_11_12_053407) do
 
   create_table "categories", force: :cascade do |t|
     t.string "title"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2020_11_10_003350) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "group"
+    t.string "username"
     t.index ["city_id"], name: "index_users_on_city_id"
   end
 
