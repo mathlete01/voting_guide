4db54193fc53ad7a8fc45b045d78ddb7f90a6288
mathlete01 <msallin@gmail.com>
2020-11-13 14:19:20 -0800

    Styled

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
         @user = User.find_by(username: params[:username])
         if @user && @user.authenticate(params[:password])
             session[:user_id] = @user.id
-            flash[:success] = "Welcome back, #{@user.username}"
+            #flash[:success] = "Welcome back, #{@user.username}"
             redirect_to decisions_path
         else
             flash[:danger] = "Incorrect credentials provided"
